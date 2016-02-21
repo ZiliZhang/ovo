76a111e8b8668e4846410f4d39694c62d3137606
www <www>
2016-02-21 05:42:03 -0500

    modified models.py

--- hello/models.py
+++ hello/models.py
@@ -2,16 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-class User2(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-class User3(models.Model):
-    first_name = models.CharField(max_length=30)
-    last_name = models.CharField(max_length=30)
-class User4(models.Model):
+class User5(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
 
