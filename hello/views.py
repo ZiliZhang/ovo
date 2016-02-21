53d102f22dda52f70aad62fe03ed5b2431312736
www <www>
2016-02-21 02:41:19 -0500

    random

--- hello/views.py
+++ hello/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Greeting, User
+#from .models import User
 
 # Create your views here.
 def index(request):
