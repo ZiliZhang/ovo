b9604ea29362e275844bfa2f3efa40376eba947f
Dongkyu (Jason) Kim <dongkyukim@Dongs-MacBook-Pro.local>
2016-02-21 05:58:32 -0500

    diff settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -1,34 +1,34 @@
 """
 Django settings for gettingstarted project, on Heroku. Fore more info, see:
 https://github.com/heroku/heroku-django-template
-
+​
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/topics/settings/
-
+​
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.8/ref/settings/
 """
-
+​
 import os
 import dj_database_url
-
-
+​
+​
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
-
+​
+​
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
+​
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
-
+​
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-
+​
+​
 # Application definition
-
+​
 INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'hello'
 )
-
+​
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -49,9 +49,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 )
-
+​
 ROOT_URLCONF = 'gettingstarted.urls'
-
+​
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -68,58 +68,68 @@ TEMPLATES = [
         },
     },
 ]
-
+​
 WSGI_APPLICATION = 'gettingstarted.wsgi.application'
-
-
+​
+​
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
+​
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'd6ah7uccpoflij',
+    'USER': 'twweybplmunsab',
+    'PASSWORD': '7P1Z1cwitwBRRDAo_TWQ5Y-hgX',
+    'HOST': 'ec2-75-101-163-171.compute-1.amazonaws.com',
+    'PORT': '5432',
+  }
+}
+​
 #DATABASES = {
 #    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'd6ah7uccpoflij',
-#	'USER': 'twweybplmunsab',
-#	'PASSWORD': '7P1Z1cwitwBRRDAo_TWQ5Y-hgX',
-#	'HOST': 'ec2-75-101-163-171.compute-1.amazonaws.com',
-#	'PORT': '5432',
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'mydb',
+#        'USER': 'ovo',
+#        'PASSWORD': 'zaq1xsw2',
+#        'HOST': '127.0.0.1',
+#        'PORT': '5432',
 #    }
 #}
-
+​
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
-
+​
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
-
-
+​
+​
 # Parse database configuration from $DATABASE_URL
 DATABASES['default'] =  dj_database_url.config(default='postgres://twweybplmunsab:7P1Z1cwitwBRRDAo_TWQ5Y-hgX@ec2-75-101-163-171.compute-1.amazonaws.com:5432/d6ah7uccpoflij')
-
+​
 # Enable Persistent Connections
 DATABASES['default']['CONN_MAX_AGE'] = 500
-
+​
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
+​
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
-
+​
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
-
+​
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
+​
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
+​
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
