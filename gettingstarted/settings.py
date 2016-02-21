62f2c9ea9c92b2fd29f76326111fdfb0c4ed3213
Dongkyu (Jason) Kim <dongkyukim@Dongs-MacBook-Pro.local>
2016-02-21 05:56:47 -0500

    diff settings.py

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -75,16 +75,16 @@ WSGI_APPLICATION = 'gettingstarted.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd6ah7uccpoflij',
-	'USER': 'twweybplmunsab',
-	'PASSWORD': '7P1Z1cwitwBRRDAo_TWQ5Y-hgX',
-	'HOST': 'ec2-75-101-163-171.compute-1.amazonaws.com',
-	'PORT': '5432',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'd6ah7uccpoflij',
+#	'USER': 'twweybplmunsab',
+#	'PASSWORD': '7P1Z1cwitwBRRDAo_TWQ5Y-hgX',
+#	'HOST': 'ec2-75-101-163-171.compute-1.amazonaws.com',
+#	'PORT': '5432',
+#    }
+#}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
