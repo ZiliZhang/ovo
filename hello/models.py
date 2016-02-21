49ed09b9dd7a8ab6eb56c96b32d15890d4cadf9f
www <www>
2016-02-21 03:02:35 -0500

    idk what im doing

--- hello/models.py
+++ hello/models.py
@@ -5,3 +5,10 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+class User2(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+class User3(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+
